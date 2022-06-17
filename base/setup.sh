f664bb00a6c140a39b7b458434dd4dcfac5b1f55
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2022-06-17 14:08:34 -0500

    fix device syntax

--- base/setup.sh
+++ base/setup.sh
@@ -1,9 +1,9 @@
-device="/dev/ttyACM0" 
+device="ttyACM0" 
 protocol=27
 enabled_constellations="GPS"
 disabled_constellations="GLONASS BEIDOU GALILEO SBAS" 
 
-ubx="ubxtool --device=$device -P $protocol"
+ubx="ubxtool --device=/dev/$device -P $protocol"
 
 
 
