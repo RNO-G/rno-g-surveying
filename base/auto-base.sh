faf7612d20fcab334d105418886d1e40fab4d699
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2022-06-17 16:30:22 -0500

    auto base station script

---
+++ base/auto-base.sh
@@ -0,0 +1,23 @@
+#! /bin/sh 
+
+# This takes data in 1 hour chunks, comrpesses, copies to server, and deletes
+
+user=rno-g
+host=rno-g
+destbase=/data/gps-base/auto-base
+
+while true ; 
+do 
+out=`date -u "+%Y/%m/%d/%Y-%m-%d/%H%M%S"`.ubx 
+dir= $(dirname ${out})
+f=$(basename ${out}) 
+mkdir -p $dir
+./ppk_base_station.sh $out 3600 
+gzip $out
+ssh $user@$host "mkdir -p $destbase/$dir"
+rsync -a $out.gz $user@$host:$destbase/$dir && rm $out
+done 
+
+  
+
+
