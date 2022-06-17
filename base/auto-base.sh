c095cd2bcc44aea812f57f53fc95cf10633c9a76
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2022-06-17 16:31:40 -0500

    small fixes

--- base/auto-base.sh
+++ base/auto-base.sh
@@ -8,14 +8,15 @@ destbase=/data/gps-base/auto-base
 
 while true ; 
 do 
-out=`date -u "+%Y/%m/%d/%Y-%m-%d/%H%M%S"`.ubx 
+out=`date -u "+%Y/%m/%d/%Y-%m-%d.%H%M%S"`.ubx 
 dir= $(dirname ${out})
 f=$(basename ${out}) 
 mkdir -p $dir
 ./ppk_base_station.sh $out 3600 
 gzip $out
 ssh $user@$host "mkdir -p $destbase/$dir"
-rsync -a $out.gz $user@$host:$destbase/$dir && rm $out
+rsync -a $out.gz $user@$host:$destbase/$dir && rm $out && rmdir -p $dir
+
 done 
 
   
