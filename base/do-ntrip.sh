1a3597ef182dc8b117be50277a2daca5a6ad78a8
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2022-06-18 00:16:45 -0500

    first attempt at ntrip server

---
+++ base/do-ntrip.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+# this assumes we've set up raw messages already and such. If you haven't, then run ppk-base-station.sh once... 
+
+
+
+#navigate to our directory if we're not there 
+fullpath=$(readlink -f $0) 
+dir=$(dirname $fullpath)
+cd $dir 
+
+# check to make sure we've compiled ntripserver
+make -C ntripserver
+
+# we need to grab the rtcm3 messages
+gpsipe -R | str2str | ntripserver/ntripserver -f -O 1
+
+
+
+
+
