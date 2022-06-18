80a9ec31d6c5e99ef9ec6b2ec6da782af141051c
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2022-06-18 00:16:27 -0500

    allow running from other paths

--- base/auto-base.sh
+++ base/auto-base.sh
@@ -6,6 +6,9 @@ user=rno-g
 host=rno-g
 destbase=/data/gps-base/auto-base
 
+fullpath=$(readlink -f $0) 
+fulldir=$(dirname $fullpath)
+
 while true ; 
 do 
 out=`date -u "+%Y/%m/%d/%Y-%m-%d.%H%M%S"`.ubx 
@@ -14,7 +17,7 @@ dir=$(dirname ${out})
 echo $dir
 f=$(basename ${out}) 
 mkdir -p $dir
-./ppk-base-station.sh $out 3600 
+${fulldir}/ppk-base-station.sh $out 3600 
 gzip $out
 ssh $user@$host "mkdir -p $destbase/$dir"
 rsync -a $out.gz $user@$host:$destbase/$dir && rm $out.gz && rmdir -p $dir
